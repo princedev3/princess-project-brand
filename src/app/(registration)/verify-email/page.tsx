437860d83c0f058e670b87029fd0445a83f0a1da
imene prince <marvinprince232@gmail.com>
2025-04-30 12:47:50 +0300

    vrify

--- src/app/(registration)/verify-email/page.tsx
+++ src/app/(registration)/verify-email/page.tsx
@@ -8,13 +8,13 @@ const VerifyEmail = () => {
     <SlideIn>
       <div className=" min-h-[calc(100vh-140px)] w-full flex justify-center items-center ">
         <div className="grid gap-y-4 w-full max-w-5xl mx-auto shadow p-7 rounded-2xl">
-          <Image
-            src={"/logo.svg"}
-            alt=""
-            width={50}
-            height={50}
-            className="w-[50px] h-[50px] mx-auto"
-          />
+        <Image
+              src={"/logo.png"}
+              alt=""
+              width={80}
+              height={80}
+              className="w-[80px] object-cover h-[80px] mx-auto"
+            />
           <VerifyEmailRegistration />
         </div>
       </div>
